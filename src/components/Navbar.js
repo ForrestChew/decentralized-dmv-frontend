e54dfc4f149ce9602a24a203af711f5f1355d98a
Forrest Chew <forrestchew@gmail.com>
2022-07-09 21:51:46 -0700

    New website title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
-import CryptoAuth from '../Auth/CryptoAuth';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import CryptoAuth from "../Auth/CryptoAuth";
 
 const Navbar = () => {
   return (
     <nav className="flex flex-row w-screen h-20 bg-white border-b-2 border-gray-400 justify-between">
       <Link to="/" className="m-6 text-lg hover:text-button">
-        <em>my</em>
-        <b>Website</b>
+        <em>Decentralized DMV</em>
       </Link>
       <div>
         <div className="btn-header">
           <Link to="/">Home</Link>
         </div>
-        {/* <AuthButton className="btn-header" /> */}
         <CryptoAuth className="btn-header" />
       </div>
     </nav>
